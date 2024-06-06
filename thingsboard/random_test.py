017ffcbc0c6103ca99be8b7ccec6d1c68ebbf86d
Yuuki <bassparty75@gmail.com>
2024-06-06 11:30:11 +0700

    update random to covert data to csv

--- thingsboard/random_test.py
+++ thingsboard/random_test.py
@@ -1,12 +1,36 @@
-import random
-from collections import Counter
+import csv
+import requests
 
-# Generate a list of random numbers
-random_numbers = [random.randint(1, 10) for _ in range(100)]
+url = "http://localhost:8080/api/plugins/telemetry/DEVICE/a1a4f640-18d8-11ef-89bf-db5a56a39248/values/timeseries"
+headers = {
+    'Content-Type': 'application/json',
+    'X-Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiYjNhZTBiNTAtMThjMC0xMWVmLThmYTItMGZlNzYyOWRlNjBlIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiI5MDU3ODk3Yi1kYzhjLTRjZjAtOTgyMC1iYjg1MmM4ODlhM2EiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTcxNzY0NjA4MiwiZXhwIjoxNzE3NjU1MDgyLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiYjM3N2I5MTAtMThjMC0xMWVmLThmYTItMGZlNzYyOWRlNjBlIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.p6geRiJ8lgDs2zUDGDYD-T_5frESwmjGXLYYA7eE1Sq5rjNNPQ23H1Ma6q5HWNJnzRVHx7ibRAmtGMGAWVu9Vg'
+}
+params = {
+    'keys': 'product_id, product_name',
+    'startTs': 1579735870785,
+    'endTs': 1717637830632
+}
 
-# Count the occurrences of each number
-counter = Counter(random_numbers)
+response = requests.get(url, headers=headers, params=params)
+data = response.json()
 
-# Print the random numbers and their counts
-print("Random Numbers:", random_numbers)
-print("Number Counts:", counter)
+with open('telemetry_data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['timestamp', 'key1', 'value1', 'key2', 'value2'])  # Write the header
+
+    for keys, values in data.items():
+        for entry in values:
+            writer.writerow([entry['ts'], keys, entry['value']])
+
+# with open('telemetry_data.csv', mode='w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(['timestamp', 'product_id', 'product_name'])  # Write the header
+
+#     for key, values in data.items():
+#         for entry in values:
+#             timestamp = entry['ts']
+#             value_list = entry['value'].strip(',')
+#             product_id = value_list[0].strip()
+#             product_name = value_list[1].strip() if len(value_list) > 1 else ''
+#             writer.writerow([timestamp, product_id, product_name])
--- thingsboard/test.py
+++ thingsboard/test.py
@@ -19,7 +19,6 @@ def pullProductName() :
     product = [["1", "Bread", 34.6777116, 135.403636, "คุณ A", 1716955754591, 1716972828257], ["2", "Snack", 34.6777116, 135.403636, "คุณ B", 1716958441447, 1716972828257], ["3", "M&M", 35.6684103, 139.5760574, "คุณ C", 1716958459496, 1716972828257], ["4", "Chocolate", 33.5598967, 130.355533, "คุณ D", 1716958477722, 1716972828257], ["5", "huh", 35.634426, 139.3485075, "คุณ E", 1716958486692, 1716972828257]]
     randomProduct = random.choice(product)
     return {"product_id" : randomProduct[0], "product_name" : randomProduct[1], "latitude" : randomProduct[2], "longitude" : randomProduct[3], "name" : randomProduct[4], "create_date" : randomProduct[5], "upload_date" : randomProduct[6]}
- 
 
 
 # response = requests.post(url, headers=headers, data=json.dumps(data))
@@ -40,4 +39,4 @@ def send_telemetry_data(data):
 while True:
     telemetry_data = pullProductName()
     send_telemetry_data(telemetry_data)
-    time.sleep(60)  # Send data every 60 seconds
+    time.sleep(5)  # Send data every 60 seconds
